e2b5bbc662f26eb5f41e4f18e4bc87e2903a8397
Marcelo Alves Nogueira <mclg.nogueira@gamil.com>
2021-09-10 22:30:22 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -24,11 +24,6 @@ import { BASE_URl } from 'utils/request';
     });
   }, []);
 
-  // const mockData = {
-  //   series: [477138, 499928, 444867, 220426, 473088],
-  //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  // }
-    
   const options = {
     legend: {
       show: true
